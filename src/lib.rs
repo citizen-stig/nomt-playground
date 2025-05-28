2843b52bdafff7b8ef583141b671b0ee5e58d836
Nikolai Golub <nikolay.v.golub@gmail.com>
2025-05-28 14:40:46 +0200

    Having test that reproduces stucking behaviour with tokio

--- src/lib.rs
+++ src/lib.rs
@@ -319,9 +319,10 @@ pub fn run_test_case(test_case: TestCase) {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use nomt::Overlay;
     use nomt::trie::KeyPath;
+    use nomt::{KeyReadWrite, Overlay};
     use std::collections::VecDeque;
+    use std::sync::{Arc, Mutex};
 
     #[test]
     fn test_single_node_rounds() {
@@ -685,6 +686,135 @@ mod tests {
         }
 
         background_task_handle.join().unwrap();
+        println!("Completed");
+    }
+
+    #[derive(Clone)]
+    struct ClonableSession {
+        session: Arc<Mutex<Option<Session<Sha2Hasher>>>>,
+    }
+
+    impl ClonableSession {
+        fn new(session: Session<Sha2Hasher>) -> Self {
+            Self {
+                session: Arc::new(Mutex::new(Some(session))),
+            }
+        }
+
+        fn finish(self, actuals: Vec<(KeyPath, KeyReadWrite)>) -> FinishedSession {
+            let mut session_lock = self.session.lock().unwrap();
+            let session = session_lock.take().unwrap();
+
+            session.finish(actuals).unwrap()
+        }
+
+        fn read(&self, key: KeyPath) -> Option<Vec<u8>> {
+            let session_lock = self.session.lock().unwrap();
+            let session = session_lock.as_ref().unwrap();
+            session.warm_up(key);
+            session.read(key).unwrap()
+        }
+    }
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn handling_web_thread_with_tokio() {
+        let dir = tempfile::tempdir().unwrap();
+        let mut opts = Options::new();
+        opts.path(dir.path().join("nomt_db"));
+        opts.commit_concurrency(1);
+        let nomt = Nomt::<Sha2Hasher>::open(opts).unwrap();
+
+        let key = b"key".to_vec();
+        let key_path: KeyPath = sha2::Sha256::digest(&key).into();
+
+        let blocks: u64 = 20;
+        let finality: u64 = 3;
+
+        let mut overlays = VecDeque::with_capacity(finality as usize);
+
+        let build_session = |current_overlays: &VecDeque<Overlay>| {
+            println!("Building session from {} overlays", current_overlays.len());
+            let params = SessionParams::default()
+                .overlay(current_overlays.iter().rev())
+                .unwrap()
+                .witness_mode(WitnessMode::read_write());
+            ClonableSession::new(nomt.begin_session(params))
+        };
+
+        let web_storage = build_session(&overlays);
+        let (web_session_sender, mut web_storage_receiver) =
+            tokio::sync::watch::channel((web_storage, None));
+        let (shutdown_tx, mut shutdown_rx) = tokio::sync::oneshot::channel::<()>();
+
+        let background_task_handle = tokio::spawn(async move {
+            println!(
+                "Background task started: Listening for web storage updates and shutdown signal."
+            );
+            loop {
+                tokio::select! {
+                     _ = &mut shutdown_rx => {
+                        println!("Shutdown signal received. Exiting background task loop.");
+                        break;
+                    }
+                    result = web_storage_receiver.changed() => {
+                        match result {
+                            Ok(_) => {
+                                println!("Web storage received");
+                                let (storage, expected_value) = (*web_storage_receiver.borrow()).clone();
+                                let value = storage.read(key_path);
+                                println!("Web storage actual   value: {:?}", value);
+                                println!("Web storage expected value: {:?}", expected_value);
+                                assert_eq!(expected_value, value);
+
+                            }
+                            Err(e) => {
+                                println!("Web storage error: {:?}", e);
+                            }
+                        }
+                    }
+                }
+            }
+        });
+
+        // Main thread
+        for height in 0..blocks {
+            let session = build_session(&overlays);
+
+            let expected_value = height.checked_sub(1).map(|v| v.to_be_bytes().to_vec());
+            let actual_value = session.read(key_path);
+            println!("Height {} Actual Value: {:?}", height, actual_value);
+            assert_eq!(actual_value, expected_value);
+
+            let written_value = Some(height.to_be_bytes().to_vec());
+            let accesses = vec![(key_path, nomt::KeyReadWrite::Write(written_value.clone()))];
+            let finished_session = session.finish(accesses);
+            overlays.push_back(finished_session.into_overlay());
+
+            let web_session = build_session(&VecDeque::new());
+            web_session_sender.send((web_session, None)).unwrap();
+
+            if let Some(f_height) = height.checked_sub(finality) {
+                println!(
+                    "H {}:Finalizing overlay at supposed height {}",
+                    height, f_height
+                );
+                let oldest_overlay = overlays.pop_front().unwrap();
+                oldest_overlay.commit(&nomt).unwrap();
+                println!(
+                    "H {}: Overlays after finalization: {}",
+                    height,
+                    overlays.len()
+                );
+            }
+            // 
+            // let web_session = build_session(&overlays);
+            // web_session_sender
+            //     .send((web_session, written_value))
+            //     .unwrap();
+        }
+
+        shutdown_tx.send(()).unwrap();
+        background_task_handle.await.unwrap();
         println!("Completed");
     }
 }
